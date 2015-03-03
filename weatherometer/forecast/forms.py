f46443079c2f44fb31b2ffb4c8f51e2e0a44227a
freejoe76 <jmurphy@denverpost.com>
2015-03-03 18:23:08 -0500

    Add form placeholder tempalte

--- weatherometer/forecast/forms.py
+++ weatherometer/forecast/forms.py
@@ -1,3 +1,5 @@
 from django import forms
 from django.conf import settings
 
+class ForecastForm(forms.Form):
+    pass
--- weatherometer/forecast/views.py
+++ weatherometer/forecast/views.py
@@ -1,4 +1,5 @@
 from models import * 
+from forms import ForecastForm
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.views.generic.list import ListView
@@ -7,5 +8,13 @@ import datetime
 
 def forecast_today(request):
     return object_list(request,
-            queryset=Forecast.objects.filter(date__exact=datetime.date.today),
-            paginate_by=50, allow_empty=True)
+        queryset=Forecast.objects.filter(date__exact=datetime.date.today),
+        paginate_by=50, allow_empty=True)
+
+def update_forecast(request):
+    if request.method == POST:
+        pass
+    else:
+        form = ForecastForm()
+
+    return render(request, 'forecast/form.html', {'form': form})
