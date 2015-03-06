d1e292ce58942a5c2d72fbd499a7fce28a9fed4c
freejoe76 <jmurphy@denverpost.com>
2015-03-06 18:33:30 -0500

    Pulled this management command out so it's available to others

--- weatherometer/forecast/forms.py
+++ weatherometer/forecast/forms.py
@@ -11,3 +11,36 @@ class ForecastForm(forms.ModelForm):
     class Meta:
         model = Forecast
         fields = '__all__'
+
+def update_temp_diff():
+    """ This does two major actions:
+    
+    1. Take the ForecastItem records with dates no more recent than yesterday --
+    those are the only ones we could be updating.
+    
+    2. Take the ActualTemperature records with dates that match the ForecastItem
+    dates. Compare the temperatures. Take the result and add it to the ForecastItem
+    differential field, and then save it.
+    """
+    yesterday = datetime.today().date() - timedelta(1)
+    temperature = ActualTemperature.objects.get(date=yesterday)
+    items = ForecastItem.objects.filter(differential__isnull=True)
+    i = 0
+    for item in items:
+        if item.date <= yesterday:
+            if item.type == 1:
+                differential = item.temperature - temperature.temperature_high
+            else:
+                differential = item.temperature - temperature.temperature_low
+            
+            if differential < 0:
+                differential_abs = differential * -1
+            else:
+                differential_abs = differential
+
+            item.differential = differential
+            item.differential_abs = differential_abs
+            item.save()
+            i = i + 1
+    
+        print '\n ForecastItem differentials updated. %i records updated' % ( i )
