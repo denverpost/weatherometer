e44e8dff36414d92d5427e4575698b66dce4ac79
freejoe76 <jmurphy@denverpost.com>
2015-03-06 18:19:37 -0500

    Get the backend working on this form

--- weatherometer/forecast/forms.py
+++ weatherometer/forecast/forms.py
@@ -4,10 +4,8 @@ from models import Forecast, ForecastItem
 from django.conf import settings
 
 ForecastItemFormSet = inlineformset_factory(Forecast, ForecastItem)
-#forecast = Forecast.objects.get(pk=1)
-forecast = Forecast.objects.get(pk=2)
-#forecast = Forecast.objects.create()
-formset = ForecastItemFormSet(instance=forecast)
+#forecast = Forecast.objects.get(pk=2)
+#formset = ForecastItemFormSet(instance=forecast)
 
 class ForecastForm(forms.ModelForm):
     class Meta:
--- weatherometer/forecast/views.py
+++ weatherometer/forecast/views.py
@@ -1,6 +1,6 @@
 from models import * 
 from forecaster.models import Forecaster
-from forms import ForecastForm, formset
+from forms import ForecastForm
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
@@ -26,13 +26,22 @@ def update_forecast(request):
         # The values for each forecast look like:
         # - id_date_{{item.pk}}
         # -- For each forecast item:
-        # -- id_forecastitem-{{item.pk}}-temperature
+        # -- id_forecastitem-{{item.pk}}-temperature_{{count}}
         # -- id_forecastitem-{{item.pk}}-forecast_day_{{count}} // day being 0 for today, 1 for tomorrow, 2 day after
         # -- id_forecastitem-{{item.pk}}-type_{{count}} // type being high or low
         for item in forecasters:
-            pass
+            # Create the wrapper forecast object
+            f, created = Forecast.objects.get_or_create(forecaster=item)
+            for i in [0,1]:
+                prefix = "forecastitem_%d_" % item.pk
+                temp = request.POST.get("%stemperature_%d" % (prefix, i))
+                day = request.POST.get("%sforecast_day_%d" % (prefix, i))
+                ftype = request.POST.get("%stype_%d" % (prefix, i))
+                if temp == '':
+                    continue
+                fi, created = ForecastItem.objects.get_or_create(items=f, temperature=temp, forecast_day=day, type=ftype)
     else:
         for item in forecasters:
             forms.append(ForecastForm(initial={'forecaster': item.pk}))
 
-    return render(request, 'forecast/form.html', {'forecasters': forecasters, 'forms': forms, 'formset': formset})
+    return render(request, 'forecast/form.html', {'forecasters': forecasters, 'forms': forms})
